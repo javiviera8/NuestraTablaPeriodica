762d12edbc7993084160f36c8cdac1afb8d7651b
jcortesv21 <jcortesv21@gmail.com>
2019-03-29 19:13:39 +0100

    Commit insert_elemetos para la base de datos

--- BaseDatos/Insert_elementos.sql
+++ BaseDatos/Insert_elementos.sql
@@ -1,1 +1,23 @@
-insert into elemento (NombreElemento,TipoElemento,NumeroAtomico,MasaAtomica) values ("Hidrogeno","No metal",1,1.008);
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Hidrogeno","H","No Metal",1,1.0079,"hidrogeno.avi");
+
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Helio","He","Gas noble",2,4.0026,"helio.avi");
+
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Litio","Li","Metal",3,6.941,"litio.avi");
+
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Berilio","Be","Metal",4,9.0122,"berilio.avi");
+
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Boro","B","Semi Conductor",5,10.811,"boro.avi");
+
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Carbono","C","No Metal",6,12.011,"carbono.avi");
+
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Nitrogeno","N","No Metal",7,14.007,"nitrogeno.avi");
+
+insert into elemento(nombreElemento,simbolo,tipoElemento,numeroAtomico,masaAtomica,urlVideo) 
+values ("Oxigeno","O","No Metal",8,15.999,"oxigeno.avi");
