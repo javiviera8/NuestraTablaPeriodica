63347ced0939a069a37265612266205ca9b1f12d
jcortesv21 <jcortesv21@gmail.com>
2019-05-13 18:33:12 +0200

    Commit general
    Commit con la version 7 del servidor rest (todos los get de todas las tablas completos), la version 11 de la base de datos (una pequeña modificacion en las tablas de usuario y operacion) y la version de los insert.

---
+++ BaseDatos/INSERTS/tablaperiodicadb_inserts_v2.sql
@@ -0,0 +1,385 @@
+
+
+--
+-- INSERT TABLA
+--
+ 
+insert into tablaperiodica() values();
+
+--
+-- INSERT USUARIOS
+--
+
+insert into usuario(nombre_usuario,oid_tabla_periodica) values ("jaime",1);
+
+insert into usuario(nombre_usuario,oid_tabla_periodica) values ("boti",1);
+
+insert into usuario(nombre_usuario,oid_tabla_periodica) values ("asier",1);
+
+--
+-- INSERT ELEMENTOS
+--
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Hidrogeno","H",1,"hidrogeno.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Helio","He",2,"helio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Litio","Li",3,"litio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Berilio","Be",4,"berilio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Boro","B",5,"boro.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Carbono","C",6,"carbono.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Nitrogeno","N",7,"nitrogeno.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Oxigeno","O",8,"oxigeno.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Fluor","F",9,"fluor.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Neon","Ne",10,"neon.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Sodio","Na",11,"sodio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Magnesio","Mg",12,"magnesio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Aluminio","Al",13,"alumino.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica)  
+values ("Silicio","Si",14,"silicio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Fosforo","P",15,"fosforo.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Azufre","S",16,"azufre.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cloro","Cl",17,"cloro.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Argon","Ar",18,"argon.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Potasio","K",19,"potasio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Calcio","Ca",20,"calcio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Escandio","Sc",21,"escandio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Titanio","Ti",22,"titanio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Vanadio","V",23,"vanadio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cromo","Cr",24,"cromo.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Manganeso","Mn",25,"manganeso.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Hierro","Fe",26,"hierro.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cobalto","Co",27,"cobalto.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Niquel","Ni",28,"niquel.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cobre","Cu",29,"cobre.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cinc","Zn",30,"cinc.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Galio","Ga",31,"galio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Germanio","Ge",32,"germanio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Arsenico","As",33,"arsenico.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Selenio","Se",34,"selenio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Bromo","Br",35,"bromo.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Kripton","Kr",36,"kripton.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Rubidio","Rb",37,"rubidio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Estroncio","Sr",38,"estroncio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Itrio","Y",39,"itrio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Circonio","Zr",40,"circonio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica)  
+values ("Niobio","Nb",41,"niobio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Molibdeno","Mo",42,"molibdeno.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Tecnecio","Tc",43,"tecnecio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Rutenio","Ru",44,"rutenio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Rodio","Rh",45,"rodio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Paladio","Pd",46,"paladio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Plata","Ag",47,"plata.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cadmio","Cd",48,"cadmio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Indio","In",49,"indio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Estaño","Sn",50,"estaño.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Antimonio","Sb",51,"antimonio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Telurio","Te",52,"telurio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Yodo","I",53,"yodo.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Xenon","Xe",54,"xenon.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cesio","Cs",55,"cesio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Bario","Ba",56,"bario.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Lantano","La",57,"lantano.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Cerio","Ce",58,"cerio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Praseodimio","Pr",59,"praseodimio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Neodimio","Nd",60,"neodimio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Prometio","Pm",61,"prometio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Samario","Sm",62,"samario.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Europio","Eu",63,"europio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Gadolinio","Gd",64,"gadolinio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Terbio","Tb",65,"terbio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Disprosio","Dy",66,"disprosio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Holmio","Ho",67,"holmio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Erbio","Er",68,"erbio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Tulio","Tm",69,"tulio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Iterbio","Yb",70,"iterbio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Lutecio","Lu",71,"lutecio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Hafnio","Hf",72,"hafnio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Tantalio","Ta",73,"tantalio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Wolframio","W",74,"wolframio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Renio","Re",75,"renio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica)  
+values ("Osmio","Os",76,"osmio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Iridio","Ir",77,"iridio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Platino","Pt",78,"platino.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Oro","Au",79,"oro.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Mercurio","Hg",80,"mercurio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Talio","Tl",81,"talio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Plomo","Pb",82,"plomo.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Bismuto","Bi",83,"bismuto.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Polonio","Po",84,"polonio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Astato","At",85,"astato.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Radon","Rn",86,"radon.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Francio","Fr",87,"francio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Radio","Ra",88,"radio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Actinio","Ac",89,"actinio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica)  
+values ("Torio","Th",90,"torio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Protactinio","Pa",91,"protactinio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Uranio","U",92,"uranio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Neptunio","Np",93,"neptunio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Plutonio","Pu",94,"plutonio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Americio","Am",95,"Americio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Curio","Cm",96,"curio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Berkelio","Bk",97,"berkelio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Californio","Cf",98,"californio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Einstenio","Es",99,"einstenio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Fermio","Fm",100,"fermio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Mendelevio","Md",101,"mendelevio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Nobelio","No",102,"nobelio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Lawrencio","Lr",103,"lawrencio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Rutherfordio","Rf",104,"rutherfordio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Dubnio","Db",105,"dubnio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Seaborgio","Sg",106,"seaborgio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Bohrio","Bh",107,"bohrio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Hassio","Hs",108,"hassio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Meitnerio","Mt",109,"meitnerio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Darmstadio","Ds",110,"darmstadio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Roentgenio","Rg",111,"roentgenio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Copernicio","Cn",112,"copernicio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Nihonium","Nh",113,"nihonium.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Flerovio","Fl",114,"flerovio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Moscovium","Mc",115,"moscovium.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Livermorio","Lv",116,"livermorio.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Tennessine","Ts",117,"tennessine.avi",1);
+
+insert into elemento(nombre_elemento,simbolo_elemento,numero_atomico,url_video,oid_tabla_periodica) 
+values ("Oganesson","Og",118,"oganesson.avi",1);
+
+--
+-- INSERT OPERACIONES
+--
+
+insert into operacion(oid_tabla_periodica,nombre_elemento,nombre_usuario) values(1,"Hidrogeno","jaime");
+
+insert into operacion(oid_tabla_periodica,nombre_elemento,nombre_usuario) values(1,"Cloro","boti");
+
+insert into operacion(oid_tabla_periodica,nombre_elemento,nombre_usuario) values(1,"oxigeno","asier");
